2f688cc25b71a2e1c4a7772f78a45ff219223cff
Olga <olgavla28@gmail.com>
2023-07-20 21:42:56 +0400

    LoginHelper

--- addressbook/LoginHelper.cs
+++ addressbook/LoginHelper.cs
@@ -1,12 +1,31 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
 
-namespace addressbook
+namespace WebAddressbookTests
 {
-    internal class LoginHelper
+    public class LoginHelper : HelperBase
     {
+
+
+        public LoginHelper(IWebDriver driver) : base(driver)
+        {
+            this.driver = driver;
+        }
+        public void Login(AccountData account)
+        {
+            driver?.FindElement(By.Name("user")).Clear();
+            driver?.FindElement(By.Name("user")).SendKeys(account.Username);
+            driver?.FindElement(By.Name("pass")).Click();
+            driver?.FindElement(By.Name("pass")).Clear();
+            driver?.FindElement(By.Name("pass")).SendKeys(account.Password);
+            driver?.FindElement(By.XPath("//input[@value='Login']")).Click();
+        }
     }
 }
